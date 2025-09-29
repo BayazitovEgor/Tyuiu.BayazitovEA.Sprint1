61c0b8997f1929dc0aecadcdebfdab1c78f6e53d
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-29 21:21:41 +0500

    Add project files.

---
+++ Tyuiu.BayazitovEA.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.BayazitovEA.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
+
