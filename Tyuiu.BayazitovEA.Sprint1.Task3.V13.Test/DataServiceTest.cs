b6de209a842831b86f24b7fc4199a5e29fb7c2f2
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-30 22:31:43 +0500

    Task3

---
+++ Tyuiu.BayazitovEA.Sprint1.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task3.V13.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Tyuiu.BayazitovEA.Sprint1.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 235.0;
+            var res = ds.MultiplyOfDigits(x);
+            Assert.AreEqual(41.125, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint1.Task3.V13/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task3.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x = 235.0;
+        Console.Title = "Спринт #1 | Выполгил: Баязитов Е.А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #13                                                            *");
+        Console.WriteLine("* Выполнил: Баязитов Егор Александрович | ПИНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 235.0                                                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.MultiplyOfDigits(x));
+    }
+}
