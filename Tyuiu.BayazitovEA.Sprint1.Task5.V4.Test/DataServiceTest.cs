3bf263ebf42c228ecf766784437413b91ab8a8d8
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-30 23:40:53 +0500

    Task5

---
+++ Tyuiu.BayazitovEA.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.BayazitovEA.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int t = 13257;
+            var res = ds.SecondsToHours(t);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task5.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int t = 13257;
+        Console.Title = "Спринт #1 | Выполгил: Баязитов Е.А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #23                                                            *");
+        Console.WriteLine("* Выполнил: Баязитов Егор Александрович | ПИНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.SecondsToHours(t));
+    }
+}
