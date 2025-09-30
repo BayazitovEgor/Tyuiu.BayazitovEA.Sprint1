e65f2801902a53b6cfdb46fec4a09745a8f846c9
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-30 21:52:26 +0500

    Task2

---
+++ Tyuiu.BayazitovEA.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.BayazitovEA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.BayazitovEA.Sprint1.Task2.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            var res = ds.CalculateSideSquare(x);
+            Assert.AreEqual(125, res);
+        }
+    }
+}
