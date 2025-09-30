65b72acb6bd864f76abc2e9ba221b64ee50bebe6
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-01 00:03:45 +0500

    Task6

---
+++ Tyuiu.BayazitovEA.Sprint1.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using static System.Net.Mime.MediaTypeNames;
+namespace Tyuiu.BayazitovEA.Sprint1.Task6.V13.Lib
+{
+    public class DataService : ISprint1Task6V13
+    {
+        public bool CheckWordsAlphabet(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return true;
+
+            for (int i = 0; i < value.Length - 1; i++)
+            {
+                if (value[i] > value[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint1.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.BayazitovEA.Sprint1.Task6.V13.Lib;
+namespace Tyuiu.BayazitovEA.Sprint1.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string text = "abc";
+            bool result = ds.CheckWordsAlphabet(text);
+            Assert.IsTrue(result);
+        }
+    }
+}
