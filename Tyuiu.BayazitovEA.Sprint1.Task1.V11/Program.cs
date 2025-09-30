6682f7e028241fc5032d4f75adcf76b8d92bb6e7
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-30 21:06:57 +0500

    Task1

---
+++ Tyuiu.BayazitovEA.Sprint1.Task1.V11/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BayazitovEA.Sprint1.Task1.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x = 6.0;
+        double y = 1.0;
+        Console.Title = "Спринт #1 | Выполгил: Баязитов Е.А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Баязитов Егор Александрович | ПИНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 5*x/(6*y)               *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 5*x/(6*y)                                                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x,y));
+    }
+}
